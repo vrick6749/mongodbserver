4450d34ad63a2af78d6e0a936e96d0f0104cc1ec
rick <vrick6749@gmail.com>
2020-10-10 15:54:11 +1100

    test comments

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,13 +1,18 @@
+// allows the exported function to be called in test file
 const { TestScheduler } = require('jest')
 let {repeater} = require('../anotherjsproject')
 
+// first test for this function
 test("Name for an example test", () => {
     expect("this is a truthy string").toBeTruthy();
 })
 
+// second test for the imported test which should come out correct
 test("repeater", () => {
     expect(repeater("hi",3)).toEqual("hihihihi")
 })
+
+// second test for the imported test which should come out as fail
 test("repeater", () => {
     expect(repeater("hi",3)).toEqual("wrong one")
 })
